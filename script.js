6c39b5c4a7b6e54620479a710db708d129d60063
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-30 11:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function submitform(event) {
+	event.preventDefault(); 
+   let name=document.getElementById("name").value.trim();
+   let age=document.getElementById("age").value.trim();
+	if(name==="" || age===""){
+		alert("Please enter valid details.")
+		return ;
+	}
+	let agePromise=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				resolve("Welcome, . You can vote")
+			}else{
+				reject("Oh sorry . You aren't old enough.")
+			}
+		},4000)
+	})
+	agePromise
+	.then((message)=>alert(message))
+	.catch((error)=>alert(error))
+}
